fa6b58b60c95321c933c0d5288ecb61ce0eddcf9
Zachary Shakked <zshakked@stevens.edu>
2016-05-04 14:41:07 -0400

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.set('port', process.env.PORT || 3000);
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise",
+	"Whenever possible, keep it simple.",
+];
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
+})
+
+app.get('/about', function(req, res) {
+	var randomeFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomeFortune });
+
+})
+
+//404 catch-all ahndler (middleware)
+app.use(function(req, res) {
+	res.status(404);
+	res.render('404');
+});
+
+//500 error handler (middleware)
+app.use(function(err, req, res, next) {
+	console.error(err.stack)
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
